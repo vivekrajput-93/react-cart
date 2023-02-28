5eef5b1072abcf4e1d16386f04151bf59c1b2107
vivekrajput-93 <viveksinghraajputt@gmail.com>
2023-02-28 20:25:41 +0530

    first cart site

---
+++ src/pages/shop/shop.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import {PRODUCTS} from "../../products"
+import { Product} from "../shop/product"
+import "./shop.css"
+
+export const Shop = () => {
+  return (
+    <div className="shop">
+      <div className="shopTitle">
+        <h1>Vicky Shop</h1>
+      </div>
+      <div className="products">{PRODUCTS.map((product) => (
+        <Product data={product}/>
+      ))}</div>
+    </div>
+  );
+};
---
+++ src/products.js
@@ -0,0 +1,65 @@
+import product1 from "./assets/1.png"
+import product2 from "./assets/2.png"
+import product3 from "./assets/3.png"
+import product4 from "./assets/4.png"
+import product5 from "./assets/5.png" 
+import product6 from "./assets/6.webp"
+import product7 from "./assets/7.webp"
+import product8 from "./assets/8.webp"
+
+export const PRODUCTS = [
+    {
+        id : 1,
+        productName : "Iphone",
+        price : 999.0,
+        productImage : product1,
+    },
+
+    {
+        id : 2,
+        productName : "Macbook Pro",
+        price : 1023.0,
+        productImage : product2,
+    },
+
+    {
+        id : 3,
+        productName : "Canon Camera",
+        price : 400.0,
+        productImage : product3,
+    },
+
+    {
+        product : 4,
+        productName: "Denim Jacket",
+        price : 566.0,
+        productImage : product4,
+    },
+
+    {
+        id : 5,
+        productName: "LED Light",
+        price : 433.0,
+        productImage : product5,
+    },
+
+    {
+        id : 6,
+        productName: "Hoodie",
+        price : 235.0,
+        productImage : product6,
+    },
+
+    {
+        id : 7,
+        productName: "Polo Shirt",
+        price : 384.0,
+        productImage : product7,
+    },
+    {
+        id : 8,
+        productName: "Gucci Cap",
+        price : 453.0,
+        productImage : product8,
+    },
+]
